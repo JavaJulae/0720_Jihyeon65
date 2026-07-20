363e80eb4c756932f9c8197c8670ebb2e6aa7bd1
jihyeon <dkswlgus605@naver.com>
2026-07-20 16:54:15 +0900

    스트림 사용해보기

--- function_Lambda/src/Main.java
+++ function_Lambda/src/Main.java
@@ -32,15 +32,35 @@
 //    }
 //}
 
-@FunctionalInterface
-interface Calculator {
-    int sum(int a, int b);
-}
+//@FunctionalInterface
+//interface Calculator {
+//    int sum(int a, int b);
+//}
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Calculator mc = Integer::sum;
+//        int result = mc.sum(3, 4);
+//        System.out.println(result);
+//    }
+//}
+
+//짝수만 뽑아 중복을 제거한 후에 역순으로 정렬하는 프로그램
+
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator mc = Integer::sum;
-        int result = mc.sum(3, 4);
-        System.out.println(result);
+        int[] data = {5, 6, 4, 2, 3, 1, 1, 2, 2, 4, 8};
+        int[] result = Arrays.stream(data)
+                .boxed()
+                .filter((a) -> a % 2 == 0)
+                .distinct()
+                .sorted(Comparator.reverseOrder())
+                .mapToInt(Integer::intValue)
+                .toArray();
+
+        System.out.println(Arrays.toString(result));
     }
 }
